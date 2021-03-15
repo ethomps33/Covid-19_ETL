cf3d8f48bde39932fa0ea609edef750fb9333a7b
ethomps33 <e.thomps33@gmail.com>
2021-03-15 16:30:07 -0700

    Loaded dataframes into the database

--- SQL_files/schema.sql
+++ SQL_files/schema.sql
@@ -1,15 +1,17 @@
 CREATE TABLE vaccine_type(VAERS_ID int PRIMARY KEY,
-						 VAX_TYPE varchar(20),
-						 VAX_MANU varchar(20),
-						 VAX_DOSE_SERIES varchar(20),
-						 VAX_ROUTE varchar(20),
-						 VAX_NAME varchar(50)
+						 VAX_TYPE varchar(100),
+						 VAX_MANU varchar(100),
+						 VAX_DOSE_SERIES varchar(100),
+						 VAX_ROUTE varchar(100),
+						 VAX_NAME varchar(100)
 );
 
 CREATE TABLE vaccine_symp(VAERS_ID int PRIMARY KEY,
-						 SYMPTOM1 varchar(50),
-						 SYMPTOM2 varchar(50),
-						 SYMPTOM3 varchar(50),
-						 SYMPTOM4 varchar(50),
-						 SYMPTOM5 varchar(50)
+						 SYMPTOM1 varchar(100),
+						 SYMPTOM2 varchar(100),
+						 SYMPTOM3 varchar(100),
+						 SYMPTOM4 varchar(100),
+						 SYMPTOM5 varchar(100)
 );
+SELECT * FROM vaccine_symp
+SELECT * FROM vaccine_type
