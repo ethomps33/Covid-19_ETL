51bb74dcb483a0a9d9e8acc9e9399106b4a27039
ethomps33 <e.thomps33@gmail.com>
2021-03-18 13:01:55 -0700

    Added joins to see different age ranges and number of deaths by the sex of patient

--- SQL_files/query.sql
+++ SQL_files/query.sql
@@ -1,3 +1,51 @@
+--Join on the vaccine_data and all_symp tables
 SELECT d.vaers_id, d.age_yrs, d.sex, s.symptoms, d.died FROM vaccine_data d
 INNER JOIN all_symp s
 ON d.vaers_id = s.vaers_id
+
+
+--Selects ages >18
+SELECT d.vaers_id, d.age_yrs, d.sex, s.symptoms, d.died FROM vaccine_data d
+INNER JOIN all_symp s
+ON d.vaers_id = s.vaers_id
+WHERE d.age_yrs < 18
+
+--Selects ages 18-35
+SELECT d.vaers_id, d.age_yrs, d.sex, s.symptoms, d.died FROM vaccine_data d
+INNER JOIN all_symp s
+ON d.vaers_id = s.vaers_id
+WHERE d.age_yrs >= 18
+AND d.age_yrs <=35
+
+--Selects ages 36-50
+SELECT d.vaers_id, d.age_yrs, d.sex, s.symptoms, d.died FROM vaccine_data d
+INNER JOIN all_symp s
+ON d.vaers_id = s.vaers_id
+WHERE d.age_yrs >= 36
+AND d.age_yrs <=50
+
+--Selects ages 51-75
+SELECT d.vaers_id, d.age_yrs, d.sex, s.symptoms, d.died FROM vaccine_data d
+INNER JOIN all_symp s
+ON d.vaers_id = s.vaers_id
+WHERE d.age_yrs >= 51
+AND d.age_yrs <=75
+
+--Selects ages 75+
+SELECT d.vaers_id, d.age_yrs, d.sex, s.symptoms, d.died FROM vaccine_data d
+INNER JOIN all_symp s
+ON d.vaers_id = s.vaers_id
+WHERE  d.age_yrs > 75
+
+--Selects where patients died
+SELECT d.vaers_id, d.age_yrs, d.sex, s.symptoms, d.died FROM vaccine_data d
+INNER JOIN all_symp s
+ON d.vaers_id = s.vaers_id
+WHERE  d.died = 'Y'
+
+--Selects the number of deaths by sex
+SELECT d.sex, COUNT(d.died) as number_of_deaths FROM vaccine_data d
+INNER JOIN all_symp s
+ON d.vaers_id = s.vaers_id
+WHERE  d.died = 'Y'
+GROUP BY d.sex
